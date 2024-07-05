e36cc5ec819c6798b10b37e3e9e5e85d20e61af4
Dominik Gadzina <epros98@gmail.com>
2024-07-05 16:16:59 +0200

    installed axios

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,10 +1,14 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home/Home";
 import Articles from "../../pages/Articles/Articles";
 import Messages from "../../pages/Messages/Messages";
 import MyList from "../../pages/MyList/MyList";
 import Statistics from "../../pages/Statistics/Statistics";
+
+const fetchApi = () => {
+  const API_URL = "https://api.coinlore.net/api/tickers/";
+};
 
 const MainContent = () => {
   return (
