6c9a89c203eee23e6e9a884d1bdbff4cebf51dda
Dominik Gadzina <epros98@gmail.com>
2024-07-13 15:01:08 +0200

    changed ui

--- src/components/Crypto/Crypto.jsx
+++ src/components/Crypto/Crypto.jsx
@@ -1,19 +1,32 @@
 import React from "react";
-
-const Crypto = ({ name, change, symbol, high24h, low24h, logo }) => {
+import "./Crypto.css";
+const Crypto = ({ name, change, symbol, high24h, low24h, logo, rank }) => {
   const changeColor = change >= 0 ? "green" : "red";
+
+  // const sum = high24h + low24h / 2;
 
   return (
     <div className="item-container">
-      <div className="item">
-        <img src={logo} alt={`${name} logo`} />
+      <div className="logo">
+        <img height={50} width={50} src={logo} alt={`${name} logo`} />
       </div>
       <div className="item-desc">
-        <p>{name}</p>
-        <p>{symbol}</p>
-        <p>{high24h}</p>
-        <p>{low24h}</p>
-        <p style={{ color: changeColor }}>{change}%</p>
+        <span>
+          <p>Current Rank:</p>
+          <p>{rank}</p>
+        </span>
+        <span>
+          <p>Name</p>
+          <p>{name}</p>
+          <p>{symbol}</p>
+        </span>
+        <span>
+          <p>{high24h}</p>
+        </span>
+        <span>
+          <p>24h</p>
+          <p style={{ color: changeColor }}>{change}</p>
+        </span>
       </div>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,16 @@ import Crypto from "../../components/Crypto/Crypto";
 const Home = () => {
   const { cryptoData, loading, error } = useContext(CryptoContext);
 
+  const formatPercentage = (value) => {
+    let numberValue = parseFloat(value);
+    return `${numberValue.toFixed(2)}`;
+  };
+
+  const formatPercentageLost = (v) => {
+    let value = parseFloat(v);
+    return `${value.toFixed(7)}`;
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -22,14 +32,12 @@ const Home = () => {
   const sortedByHigh = [...cryptoData].sort((a, b) => b.high24h - a.high24h);
   const top6Highest = sortedByHigh.slice(0, 4);
 
-  // Sort by low24h for lowest prices
   const sortedByLow = [...cryptoData].sort((a, b) => a.low24h - b.low24h);
   const top6Lowest = sortedByLow.slice(0, 4);
 
-  // Select 6 random cryptos for display
   const randomCryptos = [...cryptoData]
     .sort(() => 0.5 - Math.random())
-    .slice(0, 8);
+    .slice(0, 7);
 
   const sortedByPercentChange24h = [...cryptoData].sort(
     (a, b) => b.quote.quotes_percentChange24h - a.quote.quotes_percentChange24h
@@ -43,16 +51,20 @@ const Home = () => {
         <div className="block border-top-gainers">
           <h2>TRENDING NOW</h2>
           <div className="items-container">
-            {top6Trending.map((crypto, index) => (
+            {top6Trending.map((crypto) => (
               <Crypto
                 key={crypto.name}
+                rank={`${crypto.rank}`}
                 id={crypto.id}
                 name={crypto.name}
                 logo={crypto.logo}
-                symbol={crypto.symbol}
-                high24h={crypto.high24h}
-                low24h={crypto.low24h}
-                change={crypto.quote.quotes_percentChange24h}
+                high24h={`Highest price is: ${formatPercentage(
+                  crypto.high24h
+                )}`}
+                low24h={`Lowest price is: ${crypto.low24h}`}
+                change={`${formatPercentage(
+                  crypto.quote.quotes_percentChange24h
+                )}`}
               />
             ))}
           </div>
@@ -60,16 +72,19 @@ const Home = () => {
         <div className="block border-top-losers">
           <h2>BIGGEST LOSSES</h2>
           <div className="items-container">
-            {top6Lowest.map((crypto, index) => (
+            {top6Lowest.map((crypto) => (
               <Crypto
                 key={crypto.name}
+                rank={`Currently rank is: ${crypto.rank}`}
                 id={crypto.id}
                 name={crypto.name}
                 logo={crypto.logo}
-                symbol={crypto.symbol}
-                high24h={crypto.high24h}
-                low24h={crypto.low24h}
-                change={crypto.quote.quotes_percentChange24h}
+                high24h={`Highest price is: ${formatPercentage(
+                  crypto.high24h
+                )}`}
+                change={`${formatPercentageLost(
+                  crypto.quote.quotes_percentChange24h
+                )}`}
               />
             ))}
           </div>
@@ -77,35 +92,38 @@ const Home = () => {
         <div className="block border-top-wins">
           <h2>BIGGEST WINS</h2>
           <div className="items-container">
-            {top6Highest.map((crypto, index) => (
+            {top6Highest.map((crypto) => (
               <Crypto
                 key={crypto.name}
+                rank={`Currently rank is: ${crypto.rank}`}
                 id={crypto.id}
                 logo={crypto.logo}
                 name={crypto.name}
-                symbol={crypto.symbol}
-                high24h={crypto.high24h}
-                low24h={crypto.low24h}
-                change={crypto.quote.quotes_percentChange24h}
+                high24h={`Highest price is: ${formatPercentage(
+                  crypto.high24h
+                )}`}
+                change={formatPercentage(crypto.quote.quotes_percentChange24h)}
               />
             ))}
           </div>
         </div>
       </div>
       <div className="grid-second-container">
+        <h2>RANDOM CRYPTOS</h2>
         <div className="block border-gradient">
-          <h2>RANDOM CRYPTOS</h2>
           <div className="items-container-random">
-            {randomCryptos.map((crypto, index) => (
+            {randomCryptos.map((crypto) => (
               <Crypto
+                rank={`Currently rank is: ${crypto.rank}`}
                 key={crypto.name}
                 id={crypto.id}
                 name={crypto.name}
-                symbol={crypto.symbol}
                 logo={crypto.logo}
-                high24h={crypto.high24h}
-                low24h={crypto.low24h}
-                change={crypto.quote.quotes_percentChange24h}
+                high24h={`Highest price is: ${formatPercentage(
+                  crypto.high24h
+                )}`}
+                low24h={`Lowest price is: ${crypto.low24h}`}
+                change={formatPercentage(crypto.quote.quotes_percentChange24h)}
               />
             ))}
           </div>
