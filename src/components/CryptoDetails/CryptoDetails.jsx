fb7ec60dd4e721feec2cf2276f5fc92ea594eb17
Dominik Gadzina <epros98@gmail.com>
2024-07-25 18:22:04 +0200

    projected ui for crypto details

--- src/components/CryptoDetails/CryptoDetails.jsx
+++ src/components/CryptoDetails/CryptoDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { CryptoContext } from "../CryptoContext/CryptoContext";
+import "./CryptoDetails.css";
 
 const CryptoDetails = () => {
   const { id } = useParams(); // Get the id from URL params
@@ -37,16 +38,49 @@ const CryptoDetails = () => {
   }
 
   return (
-    <div>
-      <h1>{crypto.name}</h1>
-      <img src={crypto.image} alt={crypto.id} />
-      <p>Symbol: {crypto.symbol}</p>
-      <p>Rank: {crypto.market_cap_rank}</p>
-      <p>High 24h: {crypto.high_24h}</p>
-      <p>Low 24h: {crypto.low_24h}</p>
-      <p>Current Price: {crypto.current_price}</p>
-      <p>Price Change 24h: {crypto.price_change_24h}</p>
-    </div>
+    <>
+      <div className="product-details-header">
+        <h3>Product Details</h3>
+      </div>
+      <div className="details-container">
+        <div className="details-block">
+          <div className="details-content-container">
+            <div className="block-content-container">
+              <p>Logo</p>
+              <img src={crypto.image} alt={crypto.id} className="details-img" />
+            </div>
+            <div className="block-content-container">
+              <p>Name</p>
+              <h3 className="crypto-details-name">{crypto.name}</h3>
+            </div>
+            <div className="block-content-container">
+              <p>Symbol:</p>
+              <p>{crypto.symbol}</p>
+            </div>
+            <div className="block-content-container">
+              <p>Rank:</p>
+              <p>{crypto.market_cap_rank}</p>
+            </div>
+            <div className="block-content-container">
+              <p>High 24h</p>
+              <p>{crypto.high_24h}</p>
+            </div>
+            <div className="block-content-container">
+              <p>Low 24H</p>
+              <p>{crypto.low_24h}</p>
+            </div>
+            <div className="block-content-container">
+              <p>Current Price:</p>
+              <p>{crypto.current_price}</p>
+            </div>
+            <div className="block-content-container">
+              <p>Price Change 24h: </p>
+              <p>{crypto.price_change_24h}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
