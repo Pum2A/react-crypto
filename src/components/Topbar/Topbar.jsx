4a21f3d2c9de5fdfc081c4ea08e2002b02bf152b
Dominik Gadzina <epros98@gmail.com>
2024-08-14 17:31:59 +0200

    added search to project

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import "./Topbar.css";
 import ProfileDropdown from "../Profile/ProfileDropdown";
 
-const Topbar = () => {
-  const username = "Test";
+const Topbar = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (e) => {
+    const value = e.target.value;
+    setSearchQuery(value);
+    onSearch(value); // Send the search query up to the parent component (Home)
+  };
 
   return (
     <div className="topbar">
       <div className="topbar__logo">
         <h1>CRX</h1>
       </div>
+      <input
+        type="text"
+        placeholder="Search cryptocurrencies..."
+        value={searchQuery}
+        onChange={handleSearch}
+        className="topbar__search"
+      />
       <div className="topbar__user">
-        <ProfileDropdown username={username} />
+        <ProfileDropdown username="Test" />
       </div>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { CryptoContext } from "../../components/CryptoContext/CryptoContext";
 import "./Home.css";
 import Topbar from "../../components/Topbar/Topbar";
 import UnfoldMoreIcon from "@mui/icons-material/UnfoldMore";
+
 // Lazy load the Crypto component
 const Crypto = lazy(() => import("../../components/Crypto/Crypto"));
 
@@ -10,6 +11,8 @@ const Home = () => {
   const { cryptoData, loading, error, favorites } = useContext(CryptoContext);
   const [randomCryptos, setRandomCryptos] = useState([]);
   const [remainingCryptos, setRemainingCryptos] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredCryptos, setFilteredCryptos] = useState([]);
 
   const formatPercentage = (value) => {
     let numberValue = parseFloat(value);
@@ -24,6 +27,24 @@ const Home = () => {
       setRemainingCryptos(shuffledCryptos.slice(5));
     }
   }, [cryptoData]);
+
+  useEffect(() => {
+    // Filter the cryptocurrencies based on the search query
+    if (searchQuery) {
+      const filtered = cryptoData.filter(
+        (crypto) =>
+          crypto.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          crypto.symbol.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredCryptos(filtered);
+    } else {
+      setFilteredCryptos(cryptoData); // Display all if no search query
+    }
+  }, [searchQuery, cryptoData]);
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
 
   const handleLoadMore = () => {
     if (remainingCryptos.length > 0) {
@@ -57,103 +78,140 @@ const Home = () => {
     (a, b) => b.price_change_24h - a.price_change_24h
   );
   const top6Trending = sortedByPercentChange24h.slice(0, 5);
+
   console.log("Rendering Home component with favorites:", favorites);
 
   return (
     <>
-      <Topbar />
+      <Topbar onSearch={handleSearch} />
 
       <div className="home__grid">
-        {/* Trending Now Block */}
-        <div className="home__block home__block--trending">
-          <h2 className="home__header">Trending Now</h2>
-          <div className="home__items">
-            {top6Trending.map((crypto) => (
-              <Suspense fallback={<div>Loading...</div>} key={crypto.id}>
-                <Crypto
-                  market_cap_rank={crypto.market_cap_rank}
-                  id={crypto.id}
-                  name={crypto.name}
-                  symbol={crypto.symbol}
-                  image={crypto.image}
-                  current_price={crypto.current_price}
-                  price_change_24h={formatPercentage(crypto.price_change_24h)}
-                />
-              </Suspense>
-            ))}
+        {searchQuery ? (
+          // Display search results if there's a query
+          <div className="home__block">
+            <h2 className="home__header">Search Results</h2>
+            <div className="home__items">
+              {filteredCryptos.length > 0 ? (
+                filteredCryptos.map((crypto) => (
+                  <Suspense fallback={<div>Loading...</div>} key={crypto.id}>
+                    <Crypto
+                      market_cap_rank={crypto.market_cap_rank}
+                      id={crypto.id}
+                      name={crypto.name}
+                      symbol={crypto.symbol}
+                      image={crypto.image}
+                      current_price={crypto.current_price}
+                      price_change_24h={formatPercentage(
+                        crypto.price_change_24h
+                      )}
+                    />
+                  </Suspense>
+                ))
+              ) : (
+                <div>No results found</div>
+              )}
+            </div>
           </div>
-        </div>
-
-        {/* Biggest Losses Block */}
-        <div className="home__block home__block--losers">
-          <h2 className="home__header">Biggest Losses</h2>
-          <div className="home__items">
-            {top6Lowest.map((crypto) => (
-              <Suspense fallback={<div>Loading...</div>} key={crypto.id}>
-                <Crypto
-                  market_cap_rank={crypto.market_cap_rank}
-                  id={crypto.id}
-                  name={crypto.name}
-                  symbol={crypto.symbol}
-                  image={crypto.image}
-                  current_price={crypto.current_price}
-                  price_change_24h={formatPercentage(crypto.price_change_24h)}
-                />
-              </Suspense>
-            ))}
-          </div>
-        </div>
-
-        {/* Random Cryptos Block */}
-        <div className="home__block home__block--random">
-          <h2 className="home__header">Random Cryptos</h2>
-          <div className="home__items">
-            {randomCryptos.map((crypto) => (
-              <Suspense fallback={<div>Loading...</div>} key={crypto.id}>
-                <Crypto
-                  market_cap_rank={crypto.market_cap_rank}
-                  id={crypto.id}
-                  name={crypto.name}
-                  symbol={crypto.symbol}
-                  image={crypto.image}
-                  current_price={crypto.current_price}
-                  price_change_24h={formatPercentage(crypto.price_change_24h)}
-                />
-              </Suspense>
-            ))}
-          </div>
-          {remainingCryptos.length > 0 && (
-            <>
-              <div className="load-more-btn-container">
-                <UnfoldMoreIcon
-                  onClick={handleLoadMore}
-                  className="load-more-button"
-                />
+        ) : (
+          <>
+            {/* Trending Now Block */}
+            <div className="home__block home__block--trending">
+              <h2 className="home__header">Trending Now</h2>
+              <div className="home__items">
+                {top6Trending.map((crypto) => (
+                  <Suspense fallback={<div>Loading...</div>} key={crypto.id}>
+                    <Crypto
+                      market_cap_rank={crypto.market_cap_rank}
+                      id={crypto.id}
+                      name={crypto.name}
+                      symbol={crypto.symbol}
+                      image={crypto.image}
+                      current_price={crypto.current_price}
+                      price_change_24h={formatPercentage(
+                        crypto.price_change_24h
+                      )}
+                    />
+                  </Suspense>
+                ))}
               </div>
-            </>
-          )}
-        </div>
+            </div>
 
-        {/* Your Favorites Block */}
-        <div className="home__block home__block--favorites">
-          <h2 className="home__header home__header--favorites">
-            Your Favorites
-          </h2>
-          <div className="home__items home__items--favorites">
-            {favorites.map((crypto) => (
-              <Crypto
-                key={crypto.id} // Use crypto.id as the key
-                market_cap_rank={crypto.market_cap_rank}
-                id={crypto.id}
-                name={crypto.name}
-                symbol={crypto.symbol}
-                image={crypto.image}
-                current_price={crypto.current_price}
-                price_change_24h={crypto.price_change_24h}
-              />
-            ))}
-          </div>
-        </div>
+            {/* Biggest Losses Block */}
+            <div className="home__block home__block--losers">
+              <h2 className="home__header">Biggest Losses</h2>
+              <div className="home__items">
+                {top6Lowest.map((crypto) => (
+                  <Suspense fallback={<div>Loading...</div>} key={crypto.id}>
+                    <Crypto
+                      market_cap_rank={crypto.market_cap_rank}
+                      id={crypto.id}
+                      name={crypto.name}
+                      symbol={crypto.symbol}
+                      image={crypto.image}
+                      current_price={crypto.current_price}
+                      price_change_24h={formatPercentage(
+                        crypto.price_change_24h
+                      )}
+                    />
+                  </Suspense>
+                ))}
+              </div>
+            </div>
+
+            {/* Random Cryptos Block */}
+            <div className="home__block home__block--random">
+              <h2 className="home__header">Random Cryptos</h2>
+              <div className="home__items">
+                {randomCryptos.map((crypto) => (
+                  <Suspense fallback={<div>Loading...</div>} key={crypto.id}>
+                    <Crypto
+                      market_cap_rank={crypto.market_cap_rank}
+                      id={crypto.id}
+                      name={crypto.name}
+                      symbol={crypto.symbol}
+                      image={crypto.image}
+                      current_price={crypto.current_price}
+                      price_change_24h={formatPercentage(
+                        crypto.price_change_24h
+                      )}
+                    />
+                  </Suspense>
+                ))}
+              </div>
+              {remainingCryptos.length > 0 && (
+                <>
+                  <div className="load-more-btn-container">
+                    <UnfoldMoreIcon
+                      onClick={handleLoadMore}
+                      className="load-more-button"
+                    />
+                  </div>
+                </>
+              )}
+            </div>
+
+            {/* Your Favorites Block */}
+            <div className="home__block home__block--favorites">
+              <h2 className="home__header home__header--favorites">
+                Your Favorites
+              </h2>
+              <div className="home__items home__items--favorites">
+                {favorites.map((crypto) => (
+                  <Crypto
+                    key={crypto.id} // Use crypto.id as the key
+                    market_cap_rank={crypto.market_cap_rank}
+                    id={crypto.id}
+                    name={crypto.name}
+                    symbol={crypto.symbol}
+                    image={crypto.image}
+                    current_price={crypto.current_price}
+                    price_change_24h={crypto.price_change_24h}
+                  />
+                ))}
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
